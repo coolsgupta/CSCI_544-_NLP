b58a7f645bbf27fd13ebe28a1f200cf601e52991
Sachin Gupta <cool.sgupta@gmail.com>
2020-09-11 23:06:27 -0700

    assignment_1 submission files

--- Assignment_1/full-name-predictor.py
+++ Assignment_1/full-name-predictor.py
@@ -1,5 +1,6 @@
 import csv
 import sys
+import time
 
 
 class Utils:
@@ -69,32 +70,15 @@ class Predictor:
         with open(self.test_file, 'r') as test_file:
             reader = csv.reader(test_file)
 
-            correct = 0
-            total = 0
-            res = []
-            wrong = []
-
             for row in reader:
                 prediction = self.predict_last_name(row[0].upper())
                 predicted_names.append(prediction)
                 results.append([row[0], prediction])
-
-                # todo: check code remove before submission
-                total += 1
-                if prediction == row[1]:
-                    correct += 1
-
-                else:
-                    wrong.append([prediction, row[1]])
-
-                res.append([prediction, row[1], prediction == row[1]])
-
-        return results, predicted_names, correct, total, correct/total, res, wrong
+            return results
 
 
 if __name__ == '__main__':
-    test_file_path = 'dev-key.csv'
+    test_file_path = sys.argv[1]
     last_name_predictor = Predictor(test_file_path)
-    prediction_results, predicted_full_names, correct_preds, total_preds, accuracy, res, wrongs = last_name_predictor.predict_last_names_for_names_file()
+    prediction_results = last_name_predictor.predict_last_names_for_names_file()
     Utils.write_results(prediction_results, 'full-name-output.csv')
-    print('Done')
