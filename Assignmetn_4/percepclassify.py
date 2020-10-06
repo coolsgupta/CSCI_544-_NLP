de92d409ada9e99b01e51c94f03eae991e5ae79a
Sachin Gupta <cool.sgupta@gmail.com>
2020-10-05 23:52:17 -0700

    submission code

--- Assignmetn_4/percepclassify.py
+++ Assignmetn_4/percepclassify.py
@@ -5,37 +5,60 @@ import operator
 from os import walk
 import os
 import re
+import numpy as np
 
+
+class Utils:
+    # classification tuple format = (positive/deceptive, truthful/deceptive):(1/0,1/0)
+    POSITIVE_NEGATIVE_TUPLE_INDEX = 0
+    TRUTHFUL_DECEPTIVE_TUPLE_INDEX = 1
+    POSITIVE_CLASS_LABEL = 1
+    NEGATIVE_CLASS_LABEL = -1
+    TRUTHFUL_CLASS_LABEL = 1
+    DECEPTIVE_CLASS_LABEL = -1
+    CLASS_TUPLES = [(-1, -1), (-1, 1), (1, -1), (1, 1)]
+
+    # model param keys
+    WEIGHT_MATRIX = 'weight_matrix'
+    BIAS = 'bias'
+    POSITIVE_NEGATIVE = 'positive_negative'
+    TRUTHFUL_DECEPTIVE = 'truthful_deceptive'
+    SELECTED_FEATURES = 'selected_features'
+
+    @staticmethod
+    def get_key(key_1, key_2):
+        return '{}_{}'.format(key_1, key_2)
 
 class PerceptronPredictor:
-    def __init__(self, test_data_path):
+    def __init__(self, model_file_name, test_data_path):
+        self.positive_negative_weight_key = Utils.get_key(Utils.WEIGHT_MATRIX, Utils.POSITIVE_NEGATIVE)
+        self.truthful_deceptive_weight_key = Utils.get_key(Utils.WEIGHT_MATRIX, Utils.TRUTHFUL_DECEPTIVE)
+        self.positive_negative_bias_key = Utils.get_key(Utils.BIAS, Utils.POSITIVE_NEGATIVE)
+        self.truthful_deceptive_bias_key = Utils.get_key(Utils.BIAS, Utils.TRUTHFUL_DECEPTIVE)
+
         self.test_data_path = test_data_path
-        self.model_file_name = 'nbmodel.txt'
-        self.model = self.load_model()
-        self.class_labels = ["0", "1", "2", "3"]
         self.stop_words = self.get_stopwords()
-        # self.flag = 1
-
-    def get_class(self, class_label):
-        if class_label == "0":
-            return "deceptive negative"
-
-        elif class_label == "1":
-            return "truthful negative"
-
-        elif class_label == "2":
-            return "truthful positive"
-
-        return "deceptive positive"
+        self.model_params = self.load_model(model_file_name)
+        self.selected_features_map = self.model_params[Utils.SELECTED_FEATURES]
+        self.CLASS_LABELS = {
+            (-1, -1): 'deceptive negative',
+            (-1, 1): 'truthful negative',
+            (1, -1): 'deceptive positive',
+            (1, 1): 'truthful positive'
+        }
 
     def get_stopwords(self):
         with open('stopwords.txt', 'r') as stopwords_file:
             stop_words = set(stopwords_file.read().split('\n'))
         return stop_words
 
+    def get_class(self, class_label):
+        return self.CLASS_LABELS.get(class_label)
+
     def clean_input_case_text(self, file_name):
         text = open(file_name, 'r').read()
-        custom_strip_text = re.sub('[^a-z\d\s]+', ' ', text, flags=re.IGNORECASE)
+        custom_strip_text = re.sub(r'[' + re.escape('!(),.:;?*') + ']', ' SpecialChars ', text)
+        custom_strip_text = re.sub('[^a-z\d\s]+', ' ', custom_strip_text, flags=re.IGNORECASE)
         custom_strip_text = re.sub('(\s+)', ' ', custom_strip_text).lower()
 
         text_tokens = custom_strip_text.split(' ')
@@ -46,31 +69,38 @@ class PerceptronPredictor:
 
         return cleaned_train_input
 
-    def load_model(self):
-        with open(self.model_file_name) as f:
-            model_params = json.load(f)
+    def load_model(self, model_file_name):
+        with open(model_file_name) as model:
+            model_params = json.load(model)
+            model_params[self.positive_negative_weight_key] = np.asarray(model_params[self.positive_negative_weight_key])
+            model_params[self.truthful_deceptive_weight_key] = np.asarray(model_params[self.truthful_deceptive_weight_key])
             return model_params
+
+    def create_sample_vectors(self, sample):
+        data_vector = [sample.get(self.selected_features_map[str(word_index)], 0) for word_index in range(len(self.selected_features_map.keys()))]
+        return np.asarray(data_vector)
+
 
     def predict(self, file_name):
         text_tokens = self.clean_input_case_text(file_name)
-        class_prob = self.model['class_probability']
-        prior_prob = self.model['score']
 
-        # calculate probability of each word
-        text_segment_total_prob = dict.fromkeys(self.class_labels, 0)
-
+        text_tokens_count_map = {}
         for word in text_tokens:
-            for data_class in self.class_labels:
-                if word in prior_prob[data_class]:
-                    text_segment_total_prob[data_class] += prior_prob[data_class][word]
+            if word not in text_tokens_count_map:
+                text_tokens_count_map[word] = 0
+            text_tokens_count_map[word] += 1
 
-        # posterior Probability
-        posterior_probability = {}
-        for data_class in text_segment_total_prob.keys():
-            posterior_probability[data_class] = text_segment_total_prob[data_class] + math.log(
-                class_prob[data_class])
+        # vectorize sample
+        text_vector = self.create_sample_vectors(text_tokens_count_map)
 
-        return max(posterior_probability.items(), key=operator.itemgetter(1))[0]
+        prediction = [Utils.POSITIVE_CLASS_LABEL, Utils.TRUTHFUL_CLASS_LABEL]
+        if np.sum(text_vector * self.model_params[self.positive_negative_weight_key]) + self.model_params[self.positive_negative_bias_key] < 0:
+            prediction[Utils.POSITIVE_NEGATIVE_TUPLE_INDEX] = Utils.NEGATIVE_CLASS_LABEL
+
+        if np.sum(text_vector * self.model_params[self.truthful_deceptive_weight_key]) + self.model_params[self.truthful_deceptive_bias_key] < 0:
+            prediction[Utils.TRUTHFUL_DECEPTIVE_TUPLE_INDEX] = Utils.DECEPTIVE_CLASS_LABEL
+
+        return self.CLASS_LABELS.get(tuple(prediction))
 
     def classify(self):
         output = ''
@@ -78,14 +108,14 @@ class PerceptronPredictor:
             for test_case in file_names:
                 file_name = os.path.join(dir_path, test_case)
                 if bool(re.search('.txt', file_name)):
-                    predicted_class = self.get_class(self.predict(file_name))
+                    predicted_class = self.predict(file_name)
                     predicted_class = predicted_class + " " + file_name + '\n'
                     output += predicted_class
 
-        with open('nboutput.txt', 'w') as file:
+        with open('percepoutput.txt', 'w') as file:
             file.write(str(output))
             file.close()
 
 
 if __name__ == '__main__':
-    PerceptronPredictor(sys.argv[1]).classify()
+    PerceptronPredictor(sys.argv[1], sys.argv[2]).classify()
--- Assignmetn_4/perceplearn.py
+++ Assignmetn_4/perceplearn.py
@@ -6,6 +6,7 @@ import math
 import numpy as np
 import logging
 import traceback
+import json
 
 
 class Utils:
@@ -13,10 +14,17 @@ class Utils:
     POSITIVE_NEGATIVE_TUPLE_INDEX = 0
     TRUTHFUL_DECEPTIVE_TUPLE_INDEX = 1
     POSITIVE_CLASS_LABEL = 1
-    NEGATIVE_CLASS_LABEL = 0
+    NEGATIVE_CLASS_LABEL = -1
     TRUTHFUL_CLASS_LABEL = 1
-    DECEPTIVE_CLASS_LABEL = 0
-    CLASS_TUPLES = [(0, 0), (0, 1), (1, 0), (1, 1)]
+    DECEPTIVE_CLASS_LABEL = -1
+    CLASS_TUPLES = [(-1, -1), (-1, 1), (1, -1), (1, 1)]
+
+    # model param keys
+    WEIGHT_MATRIX = 'weight_matrix'
+    BIAS = 'bias'
+    POSITIVE_NEGATIVE = 'positive_negative'
+    TRUTHFUL_DECEPTIVE = 'truthful_deceptive'
+    SELECTED_FEATURES = 'selected_features'
 
     @classmethod
     def get_class_word_map_dict(cls):
@@ -27,6 +35,10 @@ class Utils:
         return dict.fromkeys(cls.CLASS_TUPLES, 0)
 
     @staticmethod
+    def get_key(key_1, key_2):
+        return '{}_{}'.format(key_1, key_2)
+
+    @staticmethod
     def update_dict_count(dict_to_update, key_to_update):
         if key_to_update not in dict_to_update:
             dict_to_update[key_to_update] = 0
@@ -34,7 +46,7 @@ class Utils:
 
 
 class PerceptronClassifier:
-    def __init__(self, train_data_path, epochs, num_dimensions, use_averaged_perceptron=False):
+    def __init__(self, train_data_path, epochs, num_dimensions):
         self.class_labels = Utils.CLASS_TUPLES
         self.all_word_freq_map = {}
         # self.train_data_classified = Utils.get_class_word_map_dict()
@@ -50,7 +62,7 @@ class PerceptronClassifier:
         self.stop_words = self.get_stopwords()
         self.epochs = epochs
         self.num_dimensions = num_dimensions
-        self.use_averaged_perceptron = use_averaged_perceptron
+        # self.use_averaged_perceptron = use_averaged_perceptron
         self.selected_features = tuple()
         self.create_train_data()
 
@@ -61,7 +73,9 @@ class PerceptronClassifier:
 
     def clean_input_case_text(self, file_name):
         text = open(file_name, 'r').read()
-        custom_strip_text = re.sub('[^a-z\d\s]+', ' ', text, flags=re.IGNORECASE)
+        # escape_chars = re.escape('!(),.:;?*')
+        custom_strip_text = re.sub(r'[' + re.escape('!(),.:;?*') + ']', ' SpecialChars ', text)
+        custom_strip_text = re.sub('[^a-z\d\s]+', ' ', custom_strip_text, flags=re.IGNORECASE)
         custom_strip_text = re.sub('(\s+)', ' ', custom_strip_text).lower()
 
         text_tokens = custom_strip_text.split(' ')
@@ -109,7 +123,7 @@ class PerceptronClassifier:
 
         self.train_data_tokenized.append(text_token_freq_map)
         self.train_data_label_positive_negative.append(train_class_label[Utils.POSITIVE_NEGATIVE_TUPLE_INDEX])
-        self.train_data_label_truthful_deceptive.append(train_class_label[Utils.POSITIVE_NEGATIVE_TUPLE_INDEX])
+        self.train_data_label_truthful_deceptive.append(train_class_label[Utils.TRUTHFUL_DECEPTIVE_TUPLE_INDEX])
 
     def create_train_data(self):
         for (dir_path, dir_names, file_names) in walk(next(os.walk(self.train_data_path))[0]):
@@ -128,11 +142,79 @@ class PerceptronClassifier:
         self.selected_feature_indices_map = {i: x for i, x in enumerate(self.selected_features)}
         self.create_train_data_vectors()
 
-    def train_model(self):
-        # training perceptron to classify sample as Positive or Negative
+    def train_perceptron_model(self, labels, use_averaging_perceptron=False):
+        weight_matrix = np.random.rand(self.train_data_vectors[0].shape[0])
+        bias = np.random.rand(1)[0]
+        sample_indices = np.arange(len(self.train_data_vectors))
 
+        if use_averaging_perceptron:
+            averaging_params = np.zeros(self.train_data_vectors[0].shape[0])
+            beta = 0
+            count = 1
+
+            for epoch in range(self.epochs):
+                np.random.shuffle(sample_indices)
+                for sample_index in sample_indices:
+                    x_i = self.train_data_vectors[sample_index]
+                    y_i_actual = labels[sample_index]
+                    activation = y_i_actual * (np.sum(weight_matrix * x_i) + bias)
+                    if activation < 0:
+                        weight_matrix = weight_matrix + y_i_actual * x_i
+                        bias = bias + y_i_actual
+                        averaging_params = averaging_params + y_i_actual * count * x_i
+                        beta = beta + y_i_actual * count
+                    count += 1
+
+            return weight_matrix - ((1 / count) * averaging_params), bias - ((1 / count) * beta)
+
+        else:
+            for epoch in range(self.epochs):
+                np.random.shuffle(sample_indices)
+                for sample_index in sample_indices:
+                    x_i = self.train_data_vectors[sample_index]
+                    y_i_actual = labels[sample_index]
+                    calc_activation = y_i_actual*(np.sum(weight_matrix*x_i) + bias)
+
+                    if calc_activation < 0:
+                        weight_matrix += y_i_actual * x_i
+                        bias += y_i_actual
+
+            return weight_matrix, bias
+
+    def train_model(self, use_averaging_perceptron=False):
+        model_params = {
+            Utils.SELECTED_FEATURES: self.selected_feature_indices_map
+        }
+
+        # training perceptron to classify sample as Positive or Negative
+        weight_matrix, bias = self.train_perceptron_model(
+            labels=self.train_data_label_positive_negative,
+            use_averaging_perceptron=use_averaging_perceptron
+        )
+        model_params[Utils.get_key(Utils.WEIGHT_MATRIX, Utils.POSITIVE_NEGATIVE)] = list(weight_matrix)
+        model_params[Utils.get_key(Utils.BIAS, Utils.POSITIVE_NEGATIVE)] = bias
         # training perceptron to classify sample as Truthful or Deceptive
-        return
+        weight_matrix, bias = self.train_perceptron_model(
+            labels=self.train_data_label_truthful_deceptive,
+            use_averaging_perceptron=use_averaging_perceptron
+        )
+        model_params[Utils.get_key(Utils.WEIGHT_MATRIX, Utils.TRUTHFUL_DECEPTIVE)] = list(weight_matrix)
+        model_params[Utils.get_key(Utils.BIAS, Utils.TRUTHFUL_DECEPTIVE)] = bias
+
+        self.save_model(model_params, use_averaging_perceptron)
+
+    def save_model(self, model_params, use_averaging_perceptron=False):
+        model_file_name = "averagedmodel.txt" if use_averaging_perceptron else "vanillamodel.txt"
+        with open(model_file_name, 'w') as model:
+            json.dump(model_params, model)
+
 
 if __name__ == '__main__':
-    PerceptronClassifier(sys.argv[1], 500, 1000).train_model()
+    try:
+        perceptron_classifier = PerceptronClassifier(sys.argv[1], 500, 1000)
+        perceptron_classifier.train_model()
+        perceptron_classifier.train_model(use_averaging_perceptron=True)
+
+    except Exception as e:
+        logging.error(traceback.format_exc())
+
